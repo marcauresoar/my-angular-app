ff9e18c1d1a45a65b536a1f2a8da7519fd2c6b4c
Marco Aurélio <maurelio@avenuecode.com>
2017-10-30 10:36:13 -0200

    finishing the basics module

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -13,6 +13,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created';
+  serverName = 'test server';
+  userName: string = '';
+  emptyName: boolean = true;
+  serverCreated: boolean = false;
+  servers = ['TestServer 1', 'TestServer 2'];
+  displayDetails: boolean = true;
+  clicks = [];
+  logNumber = 1;
 
   constructor() {
     setTimeout(() => {
@@ -21,6 +30,35 @@ export class ServersComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+
+
+  onInputUsername(event){
+    this.emptyName = event.target.value == '' ? true : false;
+  }
+
+  clearUsername(){
+    this.userName = '';
+    this.emptyName = true;
+  }
+
+  toggleDetails(){
+    this.displayDetails = this.displayDetails == true ? false : true;
+    this.clicks.push(new Date());
+  }
+  getDetailsDisplay(){
+    return this.displayDetails;
   }
 
 }
