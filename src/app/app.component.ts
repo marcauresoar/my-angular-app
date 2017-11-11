f12cc09941fa8453feb36206a251a2579297e508
Marco <marco.aureliosoares@hotmail.com>
2017-11-11 07:40:12 -0200

    finishing forms template driven

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,33 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') form: NgForm;
   defaultQuestion: string = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted: boolean = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.form.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.form.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   // onSubmit(form: NgForm){
@@ -20,5 +44,13 @@ export class AppComponent {
 
   onSubmit(){
     console.log(this.form);
+    this.submitted = true;
+    this.user.username = this.form.value.userData.username;
+    this.user.email = this.form.value.userData.email;
+    this.user.secretQuestion = this.form.value.secret;
+    this.user.answer = this.form.value.questionAnswer;
+    this.user.gender = this.form.value.gender;
+
+    this.form.reset();
   }
 }
