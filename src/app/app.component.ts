09b596303c29438da578386f33bdbb43f6ccb42c
Marco <marco.aureliosoares@hotmail.com>
2017-11-11 08:28:05 -0200

    doing forms td assingment

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  @ViewChild('form') form: NgForm;
+  subscriptions: string[] = ['Basic', 'Advanced', 'Pro'];
+  defaultSubscription: string = 'Advanced';
+
+  onSubmit(){
+    console.log(this.form.value);
+  }
+
 }
